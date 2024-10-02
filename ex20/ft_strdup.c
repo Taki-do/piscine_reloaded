4d1bec2cedf4e89e5e24cb91db84db5a28a21ddf
Takido-NFT <malbecktao@gmail.com>
2024-10-02 17:06:49 +0200

    first commit

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taomalbe <taomalbe@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 15:57:41 by taomalbe          #+#    #+#             */
+/*   Updated: 2024/10/02 15:57:41 by taomalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dup;
+
+	i = 0;
+	len = ft_strlen(src);
+	dup = (char *)malloc((len + 1) * sizeof(char));
+	if (!dup)
+		return (NULL);
+	while (src[i])
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
